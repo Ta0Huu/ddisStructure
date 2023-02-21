5fdfaf426aa180a4039d38bb63069c133f330097
Ta0Huu <anuwatch6552@gmail.com>
2023-02-21 17:12:52 +0700

    asved

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -5,8 +5,9 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect r1,Rect r2){
+    double x1 = max(r1.x,r2.x)-min(r1.x+r1.w,r2.x+r2.w);
+    double y1 = max(r1.y-r1.h,r2.y-r2.h)-min(r1.y,r2.y);
+    if(x1*y1 <= 0)return 0;
+    return x1*y1;    
 }
